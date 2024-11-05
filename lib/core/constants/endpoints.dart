ac83faf096d5869053f9f6ff2b5a89bd4cedda14
mtaha1999 <136391443+mtaha1999@users.noreply.github.com>
2024-11-05 23:08:09 +0200

    initial commit

---
+++ lib/core/constants/endpoints.dart
@@ -0,0 +1,21 @@
+class AppEndpoints {
+  // auth
+  static String login = 'login';
+  static String register = 'register';
+  static String logout = 'logout';
+
+  // home
+  static String getSliders = 'sliders';
+  static String getBestSeller = 'products-bestseller';
+
+  // wishlist
+  static String addToWishlist = 'add-to-wishlist';
+  static String deleteFromWishlist = 'remove-from-wishlist';
+  static String getWishlist = 'wishlist';
+
+  // cart
+  static String addToCart = 'add-to-cart';
+  static String updateCart = 'update-cart';
+  static String deleteFromCart = 'remove-from-cart';
+  static String getCart = 'cart';
+}
---
+++ lib/feature/intro/splash_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:se7ety/core/functions/navigation.dart';
+import 'package:se7ety/core/services/local_storage/local_storage.dart';
+import 'package:se7ety/feature/intro/onboarding/onboarding_view.dart';
+import 'package:se7ety/feature/intro/welcome_view.dart';
+import 'package:se7ety/feature/patient/nav_bar.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      // todo: 1)
+      // Todo: check this user is a doctor or patient
+      // todo: using firebase firestore
+      // todo: using cacheing
+      String? token = AppLocalStorage.getCachedData(key: AppLocalStorage.token);
+      bool isOnboardingShown =
+          AppLocalStorage.getCachedData(key: AppLocalStorage.onboarding) ??
+              false;
+
+      if (token != null) {
+        pushAndRemoveUntil(context, PatientNavBarWidget());
+      } else {
+        if (isOnboardingShown) {
+          pushReplacement(context, const WelcomeView());
+        } else {
+          pushReplacement(context, const OnboardingView());
+        }
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/logo.png',
+          width: 250,
+        ),
+      ),
+    );
+  }
+}
